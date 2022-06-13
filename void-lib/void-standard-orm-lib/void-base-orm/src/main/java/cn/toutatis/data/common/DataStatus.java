58b68be4765227c7d10d563a8f97febfd6dcafaa
Toutatis_Gc <45747421+ToutatisGc@users.noreply.github.com>
2022-06-13 20:56:42 +0800

    添加数据枚举

--- void-lib/void-standard-orm-lib/void-base-orm/src/main/java/cn/toutatis/data/common/DataStatus.java
+++ void-lib/void-standard-orm-lib/void-base-orm/src/main/java/cn/toutatis/data/common/DataStatus.java
@@ -1,19 +1,44 @@
 package cn.toutatis.data.common;
 
+import com.baomidou.mybatisplus.annotation.EnumValue;
+
 /**
  * @author Toutatis_Gc
+ * @date 2022/6/13 20:49
  * 数据状态
  */
-public class DataStatus {
+public enum DataStatus {
 
     /**
-     * 正常数据
+     * 数据状态枚举
      */
-    public static final String LOGIC_NORMAL = "SYS_OPEN_0000";
+    SYS_OPEN("SYS_OPEN_0000","数据正常"),
+    SYS_DELETED("SYS_DELETED_0000","逻辑删除"),
+    ;
 
-    /**
-     * 逻辑删除数据
-     */
-    public static final String LOGIC_DELETED = "SYS_CLOSE_0000";
-    
+    @EnumValue
+    private String code;
+
+    private String content;
+
+    DataStatus(String code, String content) {
+        this.code = code;
+        this.content = content;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
