16b26595c68ed612f77ff84a643444ea48bfb79a
Toutatis_Gc <45747421+ToutatisGc@users.noreply.github.com>
2022-07-23 16:11:23 +0800

    front-end init

--- void-lib/void-runtime-support/void-spring-support/src/main/kotlin/cn/toutatis/xvoid/support/spring/config/orm/mybatisplus/support/VoidMybatisService.java
+++ void-lib/void-runtime-support/void-spring-support/src/main/kotlin/cn/toutatis/xvoid/support/spring/config/orm/mybatisplus/support/VoidMybatisService.java
@@ -1,7 +1,11 @@
 package cn.toutatis.xvoid.support.spring.config.orm.mybatisplus.support;
 
+import cn.toutatis.xvoid.support.SheetExportType;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * @author Toutatis
@@ -9,6 +13,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @param <T> 实体类
  */
 public interface VoidMybatisService<T> extends IService<T> {
+
+
+
 
     /**
      * @param pagingQuery 分页对象
@@ -24,5 +31,14 @@ public interface VoidMybatisService<T> extends IService<T> {
      * @return 分页结果
      */
     Page<T> getList(PagingQuery pagingQuery,T t,String mchId);
+
+    /**
+     * 导出表格数据
+     * @param response 响应对象
+     * @param sheetExportType 导出类型
+     * @param data 导出数据
+     * @param fileName 导出文件名
+     */
+    void export(HttpServletResponse response, SheetExportType sheetExportType, List<T> data, String fileName);
 
 }
