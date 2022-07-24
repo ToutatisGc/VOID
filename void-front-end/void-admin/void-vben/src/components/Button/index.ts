bc2b9f749d1f24a48af5afe978806dc7ecb1b2fd
VOID <45747421+ToutatisGc@users.noreply.github.com>
2022-07-25 02:47:39 +0800

    Delete void-front-end/void-admin/void-vben directory

--- void-front-end/void-admin/void-vben/src/components/Button/index.ts
+++
@@ -1,9 +0,0 @@
-import { withInstall } from '/@/utils';
-import type { ExtractPropTypes } from '../../../node_modules/vue';
-import button from './src/BasicButton.vue';
-import popConfirmButton from './src/PopConfirmButton.vue';
-import { buttonProps } from './src/props';
-
-export const Button = withInstall(button);
-export const PopConfirmButton = withInstall(popConfirmButton);
-export declare type ButtonProps = Partial<ExtractPropTypes<typeof buttonProps>>;
--- void-front-end/void-admin/void-vben/types/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ComputedRef, Ref } from '../node_modules/vue';
-
-export type DynamicProps<T> = {
-  [P in keyof T]: Ref<T[P]> | T[P] | ComputedRef<T[P]>;
-};
