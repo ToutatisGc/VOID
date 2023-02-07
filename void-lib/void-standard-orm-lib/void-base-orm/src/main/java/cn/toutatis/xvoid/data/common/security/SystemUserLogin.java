906f3c3134083ccc5fdecb1226d449c06a5123e7
Toutatis_Gc <gc@toutatis.cn>
2023-02-07 11:12:45 +0800

    补充部分配置

--- void-lib/void-standard-orm-lib/void-base-orm/src/main/java/cn/toutatis/xvoid/data/common/security/SystemUserLogin.java
+++ void-lib/void-standard-orm-lib/void-base-orm/src/main/java/cn/toutatis/xvoid/data/common/security/SystemUserLogin.java
@@ -31,53 +31,45 @@ import java.util.Objects;
 public class SystemUserLogin extends EntityBasicAttribute<SystemUserLogin> {
 
     @Id @TableId
-    @ApiModelProperty(
-            value="主键ID",required = true,
-            example = "0b01f8466bcf11eda9c1b827eb90cfbc")
+    @ApiModelProperty(value="主键ID",required = true, example = "0b01f8466bcf11eda9c1b827eb90cfbc")
     @Column(name="id",columnDefinition = "VARCHAR(32) COMMENT '主键ID'")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @GeneratedValue(generator = "UUID")
     private String id;
 
     @TableField("`account`")
-    @ApiModelProperty(
-            value="账号",required = true,
-            example = "admin")
+    @ApiModelProperty(value="账号",required = true, example = "admin")
     @Column(name="account",columnDefinition = "VARCHAR(32) COMMENT '账号'")
     private String account;
 
+    @Enumerated(EnumType.STRING)
+    @TableField("registryType")
+    @ApiModelProperty(value="注册类型",required = true, example = "ACCOUNT")
+    @Column(name="registryType",columnDefinition = "VARCHAR(16) COMMENT '用户类型'")
+    private RegistryType registryType;
+
     @TableField("username")
-    @ApiModelProperty(
-            value="用户名",required = true,
-            example = "Toutatis_Gc")
+    @ApiModelProperty(value="用户名",required = true, example = "Toutatis_Gc")
     @Column(name="username",columnDefinition = "VARCHAR(32) COMMENT '用户名'")
     private String username;
 
     @TableField("phoneCode")
-    @ApiModelProperty(
-            value="电话号码",required = false,
-            example = "19902351234")
+    @ApiModelProperty(value="电话号码",required = false, example = "19902351234")
     @Column(name="phoneCode",columnDefinition = "VARCHAR(11) COMMENT '电话号码'")
     private String phoneCode;
 
     @TableField("openId")
-    @ApiModelProperty(
-            value="微信公众平台ID",required = false,
-            example = "微信公众平台ID")
+    @ApiModelProperty(value="微信公众平台ID",required = false, example = "微信公众平台ID")
     @Column(name="openId",columnDefinition = "VARCHAR(64) COMMENT '微信公众平台openID'")
     private String openId;
 
     @TableField("email")
-    @ApiModelProperty(
-            value="邮箱",required = false,
-            example = "gc@toutatis.cn")
+    @ApiModelProperty(value="邮箱",required = false, example = "gc@toutatis.cn")
     @Column(name="email",columnDefinition = "VARCHAR(64) COMMENT '邮箱'")
     private String email;
 
     @TableField("secret")
-    @ApiModelProperty(
-            value="登录密文",required = true,
-            example = "******")
+    @ApiModelProperty(value="登录密文",required = true, example = "******")
     @Column(name="secret",columnDefinition = "VARCHAR(255) COMMENT '登录密文'")
     private String secret;
 
