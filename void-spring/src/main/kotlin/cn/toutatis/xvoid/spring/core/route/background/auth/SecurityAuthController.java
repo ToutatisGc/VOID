73a6e951f8d3dac6299f9e089cf7403e8d49dab7
Toutatis_Gc <gc@toutatis.cn>
2023-02-07 11:12:45 +0800

    补充部分配置

--- void-spring/src/main/kotlin/cn/toutatis/xvoid/spring/core/route/background/auth/SecurityAuthController.java
+++ void-spring/src/main/kotlin/cn/toutatis/xvoid/spring/core/route/background/auth/SecurityAuthController.java
@@ -1,12 +1,19 @@
 package cn.toutatis.xvoid.spring.core.route.background.auth;
 
+import cn.toutatis.xvoid.data.common.result.ProxyResult;
+import cn.toutatis.xvoid.data.common.result.ResultCode;
+import cn.toutatis.xvoid.data.common.security.RegistryType;
 import cn.toutatis.xvoid.spring.core.tools.ViewToolkit;
 import cn.toutatis.xvoid.support.spring.annotations.VoidController;
+import cn.toutatis.xvoid.toolkit.validator.Validator;
+import com.alibaba.fastjson.JSONObject;
 import com.github.xiaoymin.knife4j.annotations.ApiSupport;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -29,4 +36,32 @@ public class SecurityAuthController {
         return modelAndView;
     }
 
+    @ApiOperation(value="用户注册",notes="新用户注册")
+    @RequestMapping(value = "/user/registry",method = RequestMethod.POST)
+    public ProxyResult registry(@RequestParam @ApiParam("用户名") String username,
+                                @ApiParam("信息载体") JSONObject extra,
+                                @ApiParam("注册类型") RegistryType registryType){
+        boolean checkedNull = Validator.stringsNotNull(username);
+        if(checkedNull){
+            switch (registryType) {
+                case ACCOUNT -> {
+                    /*TODO 检查账号*/
+                }
+                case EMAIL -> {
+                    /*发送邮件*/
+                }
+                case PHONE -> {
+                    /*发送短信验证码*/
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + registryType);
+            }
+        }
+        return new ProxyResult(ResultCode.NORMAL_SUCCESS);
+    }
+
+    @ApiOperation(value="忘记密码",notes="忘记密码,重置用户密码")
+    @RequestMapping(value = "/login/page",method = RequestMethod.POST)
+    public ProxyResult forgetPassword(String username){
+        return new ProxyResult(ResultCode.NORMAL_SUCCESS);
+    }
 }
