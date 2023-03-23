39716b3ce7344105c74050c3d1b1def364327ff2
Toutatis_Gc <gc@toutatis.cn>
2023-03-23 18:40:56 +0800

    补充部分配置

---
+++ void-lib/void-standard-orm-lib/void-base-orm/src/main/java/cn/toutatis/xvoid/mapping/concepts/Nameable.java
@@ -0,0 +1,19 @@
+package cn.toutatis.xvoid.mapping.concepts;
+
+/**
+ * @author Toutatis_Gc
+ * 可命名的
+ */
+public interface Nameable {
+
+    /**
+     * @param name 名称
+     */
+    void setName(String name);
+
+    /**
+     * @return 命名内容
+     */
+    String getName();
+
+}
