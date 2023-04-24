6395a46bed7cecefcb4ec07498f2277e72db2678
Toutatis_Gc <gc@toutatis.cn>
2023-04-24 18:30:16 +0800

    minio测试

---
+++ void-lib/void-runtime-support/void-spring-support/src/main/kotlin/cn/toutatis/xvoid/support/spring/config/ObjectStorageMode.java
@@ -0,0 +1,19 @@
+package cn.toutatis.xvoid.support.spring.config;
+
+/**
+ * @author Toutatis_Gc
+ * @date 2022/10/26 21:16
+ */
+public enum ObjectStorageMode {
+
+    /**
+     * 存储文件至本地
+     */
+    LOCAL,
+
+    /**
+     * 存储文件至MINIO系统
+     */
+    MINIO,
+
+}
