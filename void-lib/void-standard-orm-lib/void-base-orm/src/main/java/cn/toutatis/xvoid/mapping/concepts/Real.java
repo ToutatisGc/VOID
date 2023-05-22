a266edc4b5acc68bfa0c2156529a6a38f26403df
Toutatis_Gc <gc@toutatis.cn>
2023-05-22 14:43:32 +0800

    mapping测试

---
+++ void-lib/void-standard-orm-lib/void-base-orm/src/main/java/cn/toutatis/xvoid/mapping/concepts/Real.java
@@ -0,0 +1,4 @@
+package cn.toutatis.xvoid.mapping.concepts;
+
+public interface Real {
+}
---
+++ void-lib/void-standard-orm-lib/void-base-orm/src/main/java/cn/toutatis/xvoid/mapping/concepts/animal/Human.java
@@ -0,0 +1,9 @@
+package cn.toutatis.xvoid.mapping.concepts.animal;
+
+import cn.toutatis.xvoid.mapping.appurtenant.gender.Gender;
+import cn.toutatis.xvoid.mapping.appurtenant.occupation.Occupation;
+import cn.toutatis.xvoid.mapping.concepts.Nameable;
+
+public interface Human extends Nameable, Gender, Creaturely, Occupation {
+
+}
