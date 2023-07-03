3432253ce4f24d981040440c69947a6dc17c39b3
Toutatis_Gc <gc>
2023-07-03 10:59:53 +0800

    初始化

---
+++ void-lib/void-separate-modules/void-sqlite/src/main/kotlin/cn/toutatis/xvoid/sqlite/VoidModuleInfo.java
@@ -0,0 +1,15 @@
+package cn.toutatis.xvoid.sqlite;
+
+/**
+ * @author Toutatis_Gc
+ */
+public class VoidModuleInfo {
+
+    public static final String PACKAGE_NAME = "cn.toutatis.xvoid.sqlite";
+
+    public static final String PACKAGE_PATH = "cn.toutatis.xvoid.sqlite";
+
+    public static final String MODULE_NAME = "XVOID-SQLITE";
+
+
+}
