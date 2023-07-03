8ea76d5121175268c8c309f181ca9ff22b982f16
Toutatis_Gc <gc@toutatis.cn>
2023-07-03 11:43:40 +0800

    测试

---
+++ void-lib/void-separate-modules/void-toolkit/src/main/kotlin/cn/toutatis/xvoid/toolkit/file/ArchiveToolkit.kt
@@ -0,0 +1,30 @@
+package cn.toutatis.xvoid.toolkit.file
+
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
+import java.io.FileInputStream
+
+/**
+ * Archive toolkit
+ * 压缩文件工具类
+ * @author Toutatis_Gc
+ * @constructor Create empty Archive toolkit
+ */
+object ArchiveToolkit {
+
+    @JvmStatic
+    fun extractZipArchive(filename:String): Unit {
+        try {
+            val fileInputStream = FileInputStream(filename).use {
+                val zipArchiveInputStream = ZipArchiveInputStream(it).use {
+
+                }
+            }
+
+        }catch (exception:RuntimeException){
+            exception.printStackTrace()
+        }finally {
+
+        }
+
+    }
+}
