ce6d42b9ddeb371616b1e56b9e5c22b1e17f9fe0
Toutatis_Gc <gc@toutatis.cn>
2023-07-04 18:21:06 +0800

    API document test

--- void-lib/void-separate-modules/void-third-party/src/main/kotlin/cn/toutatis/xvoid/third/party/openai/Test.java
+++ void-lib/void-separate-modules/void-third-party/src/main/kotlin/cn/toutatis/xvoid/third/party/openai/Test.java
@@ -2,17 +2,16 @@ package cn.toutatis.xvoid.third.party.openai;
 
 import cn.toutatis.xvoid.third.party.aMap.AMapAPI;
 import cn.toutatis.xvoid.third.party.basic.ApiDocumentInfo;
-import cn.toutatis.xvoid.third.party.basic.MappingEnumPropertyUtils;
-
-import java.util.HashMap;
 
 public class Test {
     //完成对象的解析
     public static void main(String[] args) {
         //info对象
 
-        ApiDocumentInfo resolve = AMapAPI.CHECK_WEATHER_FORECASTS.resolve();
-        System.err.println(resolve);
+//        ApiDocumentInfo resolve = AMapAPI.CHECK_WEATHER_FORECASTS.resolve();
+//        System.err.println(resolve);
+
+        AMapAPI.CHECK_WEATHER_FORECASTS.printAPI();
 
 //        StudyEnum.StudyEnumInfo info = StudyEnum.PHP.resolve();
 //        System.out.println(info.getCostAnno().max());
