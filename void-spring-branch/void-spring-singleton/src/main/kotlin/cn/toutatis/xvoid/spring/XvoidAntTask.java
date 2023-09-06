eddef7008d48d43adbfcf7f598d805044ade07a3
Toutatis_Gc <gc@toutatis.cn>
2023-09-06 14:36:52 +0800

    自动KEY更新预提交

---
+++ void-spring-branch/void-spring-singleton/src/main/kotlin/cn/toutatis/xvoid/spring/XvoidAntTask.java
@@ -0,0 +1,9 @@
+package cn.toutatis.xvoid.spring;
+
+public class XvoidAntTask {
+    public static void main(String[] args) {
+
+        // TODO AES KEY 自动更新
+        System.err.println(6666666);
+    }
+}
